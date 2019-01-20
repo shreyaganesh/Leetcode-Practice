12d723f5732f96e5460cf46db2ff91954e0a3fe7
Shreya Ganesaraman <sganesar@ucsd.edu>
2019-01-20 14:53:24 -0800

    FINAL WORKING CODE: Reverse Linked List

--- 206_reverse_linked_list.cpp
+++ 206_reverse_linked_list.cpp
@@ -10,21 +10,12 @@ struct ListNode{
 class Solution {
 public:
   void add_node(ListNode**, int);
-
-  ListNode* reverse_ll(ListNode* head) {
-    ListNode* current=head;
-    ListNode* prev=NULL;
-
-    while(current!=NULL) {
-      ListNode* temp=current->next;
-      current->next=prev;
-      prev=current;
-      current=temp;
-    }
-    return prev;
-  }
+  void print_List(ListNode*);
+  ListNode* reverse_ll(ListNode*);
+  ListNode* reverseList_recursion(ListNode*);
 };
 
+// Add new node to end of linked list given head and data of node
 void Solution::add_node(ListNode** head, int data) {
   ListNode* node = new ListNode(data);
   if(*head==NULL)
@@ -38,29 +29,64 @@ void Solution::add_node(ListNode** head, int data) {
   }
 }
 
-int main() {
-std::vector<int>elements{1,2,3,4,5};
-Solution linked_list;
-ListNode* head=NULL;
-for(int i = 0; i<elements.size(); i++) {
-  linked_list.add_node(&head,elements[i]);
+// Print elements of Linked List
+void Solution::print_List(ListNode* head) {
+  ListNode* current=head;
+  while(current->next!=NULL) {
+    std::cout<<current->data<<"->";
+    current=current->next;
+  }
+  std::cout<<current->data<<std::endl;
 }
-std::cout<<"LINKED LIST:\n";
-ListNode* current=head;
-while(current->next!=NULL) {
-  std::cout<<current->data<<"->";
-  current=current->next;
-}
-std::cout<<current->data<<std::endl;
 
-head=linked_list.reverse_ll(head);
-std::cout<<"REVERSED LINKED LIST:\n";
-current=head;
-while(current->next!=NULL) {
-  std::cout<<current->data<<"->";
-  current=current->next;
+// Linked List Reversal - ITERATIVE
+struct ListNode* Solution::reverse_ll(ListNode* head) {
+    ListNode* current=head;
+    ListNode* prev=NULL;
+
+    while(current!=NULL) {
+      ListNode* temp=current->next;
+      current->next=prev;
+      prev=current;
+      current=temp;
+    }
+    return prev;
+  }
+
+// Linked List Reversal - RECURSIVE
+struct ListNode* Solution::reverseList_recursion(ListNode* head) {
+  if(head==NULL || head->next==NULL)
+    return head;
+
+  ListNode* p = reverseList_recursion(head->next);
+  head->next->next = head;
+  head->next = NULL;
+  return p;
 }
-std::cout<<current->data<<std::endl;
+
+// MAIN FUNCTION CALL
+int main() {
+
+//initialize the linked list and print it
+  std::vector<int>elements{1,2,3,4,5};
+  Solution linked_list;
+  ListNode* head=NULL;
+  for(int i = 0; i<elements.size(); i++) {
+    linked_list.add_node(&head,elements[i]);
+  }
+
+  std::cout<<"LINKED LIST:\n";
+  linked_list.print_List(head);
+
+//Reverse the linked list using iterative technique
+  head=linked_list.reverse_ll(head);
+  std::cout<<"REVERSED LINKED LIST (ITERATIVE):\n";
+  linked_list.print_List(head);
+
+//reverse it back to original using recursive technique
+  head=linked_list.reverseList_recursion(head);
+  std::cout<<"REVERSED LINKED LIST (RECURSIVE):\n";
+  linked_list.print_List(head);
 
   return 0;
 }
